bd964edef04bbdc057dee61e5edbcbc30de6b8f5
HARD953 <issa97403@gmail.com>
2025-08-01 16:08:52 +0000

    first commit5

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // tes options de configuration ici
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
